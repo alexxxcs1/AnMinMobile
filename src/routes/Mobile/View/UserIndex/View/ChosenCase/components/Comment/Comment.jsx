4e41c1d9ab95f7d66802807462d193ba976357f2
rup-chinaPC <packy@rup-china.com>
2019-05-10 18:18:07 +0800

    更新

--- src/routes/Mobile/View/UserIndex/View/ChosenCase/components/Comment/Comment.jsx
+++ src/routes/Mobile/View/UserIndex/View/ChosenCase/components/Comment/Comment.jsx
@@ -5,16 +5,24 @@ import close from 'assets/close.png'
 import PropTypes from "prop-types";
 
 import commentbot from 'assets/commentbot.png'
-import commenttop from 'assets/commenttop.png'
+import commenttop1 from 'assets/commenttop1.png'
+import commenttop2 from 'assets/commenttop2.png'
+
+const COMMENTTOP=[
+  commenttop1,
+  commenttop2
+];
   
 export class Comment extends Component {
 constructor(props) {
   super(props);
   this.state = {
-    content:'',
+    select:0,
+    content:[],
   };
      this.refreshProps = this.refreshProps.bind(this);
      this.handleClose = this.handleClose.bind(this);
+     this.HandleSelect = this.HandleSelect.bind(this);
 }
 componentWillReceiveProps(nextprops) {
   this.refreshProps(nextprops);
@@ -32,16 +40,23 @@ handleClose(){
         content:null,
     })
 }
+HandleSelect(index){
+  this.setState({
+    select:index,
+  })
+}
 render() {
   return (
     <DarkBox>
         <div className={[style.CommentBox,'childcenter'].join(' ')}>
-            <img src={commenttop} className={style.CommentTopImg} alt=""/>
+            <img src={COMMENTTOP[this.state.select]} className={style.CommentTopImg} alt=""/>
             <div className={style.CloseButton} onClick={this.handleClose}>
                 <img src={close} alt=""/>
             </div>
             <div className={style.DetialBox}>
-                {this.state.content}
+                {this.state.select <= 0?'':<div className={style.LeftArrow} onClick={this.HandleSelect.bind(this,this.state.select-1)}></div>}
+                <div className={style.DetialTextArea}>{this.state.content[this.state.select]}</div>
+                {this.state.select >= this.state.content.length-1?'':<div className={style.RightArrow} onClick={this.HandleSelect.bind(this,this.state.select+1)}></div>}
             </div>
             <img src={commentbot} className={style.CommentBotImg} alt=""/>
         </div>
--- src/routes/Mobile/View/UserIndex/View/ChosenCase/components/Score/Score.jsx
+++ src/routes/Mobile/View/UserIndex/View/ChosenCase/components/Score/Score.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import style from './Score.scss'
 import {api} from 'common/app'
+
+import scoreBasis1 from 'assets/scoreBasis1.png'
+import scoreBasis2 from 'assets/scoreBasis2.png'
+import scoreBasis3 from 'assets/scoreBasis3.png'
+import scoreBasis4 from 'assets/scoreBasis4.png'
+import scoreBasis5 from 'assets/scoreBasis5.png'
+import scoreBasis6 from 'assets/scoreBasis6.png'
   
 export class Score extends Component {
 constructor(props) {
@@ -65,115 +72,71 @@ render() {
                 <div className={[style.FormColumn,'childcenter'].join(' ')}>得分</div>
             </div>
             <div className={[style.FormRow,'childcenter'].join(' ')}>
-                <div className={[style.FormColumn,'childcenter childcolumn'].join(' ')}>
-                    <span>主观资料</span> 
-                    <span>（15分）</span>
+                <div className={style.ImageColumn}>
+                    <img src={scoreBasis1} alt=""/>
                 </div>
-                <div className={[style.FormColumn,'childcenter childcolumn childalignstart'].join(' ')}>
-                    <p>1、格式：按主要健康问题，逐一描述</p>
-                    <p>2、主要书写内容</p>
-                    <p>（1）主诉</p>
-                    <p>（2）主要症状描述、病情演变</p>
-                    <p>（3）诊治经过及结果</p>
-                    <p>（4）相关病史</p>
-                    <p>（5）家族史</p>
-                    <p>（6）生活方式、心理及社会因素</p>
-                </div>
-                <div className={[style.FormColumn,'childcenter'].join(' ')}>
+                <div className={style.ScoreColumn}>
                     <div className={style.SelectBox}>
                         {this.state.scoreArray[0]}分
                     </div>
                 </div>
             </div>
             <div className={[style.FormRow,'childcenter'].join(' ')}>
-                <div className={[style.FormColumn,'childcenter childcolumn'].join(' ')}>
-                    <span>客观检查</span> 
-                    <span>（10分）</span>
+                <div className={style.ImageColumn}>
+                    <img src={scoreBasis2} alt=""/>
                 </div>
-                <div className={[style.FormColumn,'childcenter childcolumn childalignstart'].join(' ')}>
-                    <p>1、体检结果</p>
-                    <p>2、实验室检查及辅助检查等</p>
-                    <p>3、相关心理测验等其他评估</p>
-                </div>
-                <div className={[style.FormColumn,'childcenter'].join(' ')}>
+                <div className={style.ScoreColumn}>
                     <div className={style.SelectBox}>
                         {this.state.scoreArray[1]}分
                     </div>
                 </div>
             </div>
             <div className={[style.FormRow,'childcenter'].join(' ')}>
-                <div className={[style.FormColumn,'childcenter childcolumn'].join(' ')}>
-                    <span>评价</span> 
-                    <span>（10分）</span>
+                <div className={style.ImageColumn}>
+                    <img src={scoreBasis3} alt=""/>
                 </div>
-                <div className={[style.FormColumn,'childcenter childcolumn childalignstart'].join(' ')}>
-                    <p>1、主要诊断</p>
-                    <p>2、存在的危险因素与健康问题</p>
-                    <p>3、并发症或其他临床情况</p>
-                    <p>4、患者的依从性</p>
-                    <p>5、家庭可利用的资源</p>
-                </div>
-                <div className={[style.FormColumn,'childcenter'].join(' ')}>
+                <div className={style.ScoreColumn}>
                     <div className={style.SelectBox}>
                         {this.state.scoreArray[2]}分
                     </div>
                 </div>
             </div>
             <div className={[style.FormRow,'childcenter'].join(' ')}>
-                <div className={[style.FormColumn,'childcenter childcolumn'].join(' ')}>
-                    <span>处置计划</span> 
-                    <span>（15分）</span>
+                <div className={style.ImageColumn}>
+                    <img src={scoreBasis4} alt=""/>
                 </div>
-                <div className={[style.FormColumn,'childcenter childcolumn childalignstart'].join(' ')}>
-                    <p>1、进一步诊查计划</p>
-                    <p>2、治疗计划</p>
-                    <p>（1）药物治疗及相关问题</p>
-                    <p>（2）非药物治疗——行为干预计划，饮食、运动等健康教育指导、注意事项等。</p>
-                    <p>3、随访要求</p>
-                </div>
-                <div className={[style.FormColumn,'childcenter'].join(' ')}>
+                <div className={style.ScoreColumn}>
                     <div className={style.SelectBox}>
                         {this.state.scoreArray[3]}分
                     </div>
                 </div>
             </div>
             <div className={[style.FormRow,'childcenter'].join(' ')}>
-                <div className={[style.FormColumn,'childcenter childcolumn'].join(' ')}>
-                    <span>分析</span> 
-                    <span>（25分）</span>
+                <div className={style.ImageColumn}>
+                    <img src={scoreBasis5} alt=""/>
                 </div>
-                <div className={[style.FormColumn,'childcenter childcolumn childalignstart'].join(' ')}>
-                    <p>1、根据病情的分析理论和循证医学依据充分、准确</p>
-                    <p>2、论点明确、论据充分、分析准确、论证合理</p>
-                    <p>3、患儿症状体征或检查结果影像资料(照片)</p>
-                    <p>4、掌握坚实的基础理论和系统的专业知识</p>
-                </div>
-                <div className={[style.FormColumn,'childcenter'].join(' ')}>
+                <div className={style.ScoreColumn}>
                     <div className={style.SelectBox}>
                         {this.state.scoreArray[4]}分
                     </div>
                 </div>
             </div>
             <div className={[style.FormRow,'childcenter'].join(' ')}>
-                <div className={[style.FormColumn,'childcenter childcolumn'].join(' ')}>
-                    <span>总结</span> 
-                    <span>（25分）</span>
+                <div className={style.ImageColumn}>
+                    <img src={scoreBasis6} alt=""/>
                 </div>
-                <div className={[style.FormColumn,'childcenter childcolumn childalignstart'].join(' ')}>
-                    <p>1、对案例理解是否透彻</p>
-                    <p>2、病史陈述是否完整、准确 </p>
-                    <p>3、病例思考与陈述</p>
-                </div>
-                <div className={[style.FormColumn,'childcenter'].join(' ')}>
+                <div className={style.ScoreColumn}>
                     <div className={style.SelectBox}>
                         {this.state.scoreArray[5]}分
                     </div>
                 </div>
             </div>
-            <div className={[style.AllCount,'childcenter'].join(' ')}>
-                <div className={style.ScoreType} >SOAP病例描述 <span style={{color:'#DA4913'}}>{this.getCountScore('soap')}</span>  分</div>
-                <div className={style.ScoreType} >病例分析/归纳/收获/总结 <span style={{color:'#DA4913'}}>{this.getCountScore('analysis')}</span>  分</div>
-                <div className={style.ScoreType} >总计 <span style={{color:'#DA4913'}}>{this.getCountScore()}</span>  分</div>
+            <div className={[style.AllCount,'childcenter childcolumn'].join(' ')}>
+                <div className={[style.ScoreGroup,'childcenter'].join(' ')}>
+                    <div className={style.ScoreType} >SOAP病例描述 <span className={style.otherCount} style={{color:'#DA4913'}}>{this.getCountScore('soap')}</span>  分</div>
+                    <div className={style.ScoreType} >病例分析/归纳/收获/总结 <span className={style.otherCount} style={{color:'#DA4913'}}>{this.getCountScore('analysis')}</span>  分</div>
+                </div>
+                <div className={style.ScoreType} >总计 <span className={style.CountAllText} style={{color:'#DA4913'}}>{this.getCountScore()}</span>  分</div>
             </div>
         </div>
         <div onClick={(()=>{
